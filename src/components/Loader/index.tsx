68b137bcc9f3ddb0647f06b53fece29b764269b4
Rafo Petrosyan <petrosyanrafo0@gmail.com>
2024-12-08 21:23:57 +0400

    changes

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import Spinner from 'react-native-loading-spinner-overlay';
+import COLORS from '../../constants/colors.ts';
+import { fontFamilies } from '../../constants/fonts.ts';
+
+type Props = {
+   visible: boolean;
+};
+
+const Loader: React.FC<Props> = ({ visible }) => {
+   const { t } = useTranslation();
+
+   return <Spinner visible={visible} textContent={t('loading')} textStyle={styles.spinnerText} />;
+};
+
+const styles = StyleSheet.create({
+   spinnerText: {
+      fontSize: 16,
+      color: COLORS.secondaryText,
+      fontFamily: fontFamilies.MONTSERRAT['400'],
+      fontWeight: '400',
+   },
+});
+
+export default Loader;
